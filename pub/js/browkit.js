a273200f7f066454a5ab560fee3d89283058b6f1
markkulube <markkulube@users.noreply.github.com>
2020-11-25 14:12:19 -0500

    feat: create container for browkit dynamically.

--- pub/js/browkit.js
+++ pub/js/browkit.js
@@ -22,42 +22,23 @@ PaletteMaker.prototype = {
 
         paletteTools.forEach(palette_tool => {
 
-           switch (palette_tool) {
-                case "calculator":
-                    const listItm1 = document.createElement('div')
-                    listItm1.className = "tool"
-                    listItm1.innerText = "Caculator"
-                    listItm1.onclick = (e) => { 
-                                        e.preventDefault
-                                        displayCalculator()
-                                    }
-                    ulist.appendChild(listItm1)
-                    break;
-                case "stick_notes":
-                    const listItm2 = document.createElement('div')
-                    listItm2.className = "tool"
-                    listItm2.innerText = "Sticky Note"
-                    listItm2.onclick = (e) => { 
-                        e.preventDefault
-                        displayStickyButtons()
-                    }
-                    ulist.appendChild(listItm2)
-                    break;
-           
-               default:
-                   break;
-           }
+          const listItm = document.createElement('div')
+          listItm.className = palette_tool.class_name
+          listItm.innerText = palette_tool.inner_text
+          listItm.onclick = palette_tool.display_fcn
+          ulist.appendChild(listItm)
+
        });
 
         palette.appendChild(ulist) 
 
         const containerDiv = document.createElement('div')
-        containerDiv.className = "container"
-        containerDiv.id = 'container'
+        containerDiv.className = "browkit_container"
+        containerDiv.id = 'browkit_container'
         containerDiv.appendChild(palette)
         
         document.querySelector('body').appendChild(containerDiv)
-        const container = document.querySelector('.container');
+        const container = document.querySelector('.browkit_container');
         container.addEventListener('mousedown', userPressed);
 
         function userPressed(event) {
@@ -72,41 +53,3 @@ PaletteMaker.prototype = {
 
 	}
 }
-
-
-
-/* const containerDiv = document.createElement('div')
-containerDiv.className = "container"
-const boxDiv = document.createElement('div')
-boxDiv.className = 'box'
-containerDiv.appendChild(boxDiv)
-document.querySelector('body').appendChild(containerDiv)
-const container = document.querySelector('.container');
-container.addEventListener('pointerdown', userPressed, { passive: true });
-
-var element, bbox, startX, startY;
-
-function userPressed(event) {
-  element = event.target;
-  if (element.classList.contains('box')) {
-    startX = event.clientX;
-    startY = event.clientY;
-    bbox = element.getBoundingClientRect();
-    container.addEventListener('pointermove', userMoved, { passive: true });
-    container.addEventListener('pointerup', userReleased, { passive: true });
-    container.addEventListener('pointercancel', userReleased, { passive: true });
-  };
-};
-
-function userMoved(event) {
-  let deltaX = event.clientX - startX;
-  let deltaY = event.clientY - startY;
-  element.style.left = bbox.left + deltaX + "px";
-  element.style.top = bbox.top + deltaY + "px";
-};
-
-function userReleased(event) {
-  container.removeEventListener('pointermove', userMoved);
-  container.removeEventListener('pointerup', userReleased);
-  container.removeEventListener('pointercancel', userReleased);
-};  */
